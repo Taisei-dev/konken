1349e1208fdb6e6359e297b71f533bbc71875357
taisei-dev <butsurint@gmail.com>
2022-05-05 18:54:41 +0900

    update

--- api/stop.py
+++ api/stop.py
@@ -1,4 +1,3 @@
-from urllib import response
 from flask import Flask,request,render_template
 import urllib.request,urllib.parse
 import json
@@ -10,7 +9,13 @@ def stop():
     if request.args.get('stationsid') != None:
         stationsid=request.args.get('stationsid')
     req=urllib.request.Request('https://kuruken.jp/Approach/ChangeNoriba',json.dumps({'selectLang':'ja','stationSid':stationsid,'goalStaCode':'','goalComId':'','listSortMode':'1'}).encode(),{'Content-Type': 'application/json'})
-    return render_template('stop.html',response=urllib.request.urlopen(req).read().decode().replace("\\r\\n","\n").replace("\\u003c","<").replace("\\u003e",">").replace('\\"','"').replace("\\u0027","'").replace("<script","<!--<script").replace("</script>","</script>-->")[1:-1])
+    kurukendata=urllib.request.urlopen(req).read().decode().replace("\\r\\n","\n").replace("\\u003c","<").replace("\\u003e",">").replace('\\"','"').replace("\\u0027","'").replace("<script","<!--<script").replace("</script>","</script>-->")[1:-1]
+    req=urllib.request.Request('https://kuruken.jp/Approach/StationInfo?sid='+stationsid,data=json.dumps({'Content-Type': 'application/json'}).encode())
+    stationinfo=json.loads(urllib.request.urlopen(req).read().decode())
+    req=urllib.request.Request('https://kuruken.jp/Approach/GetStationsForModalDialogByNoriba?selectLang=ja&goalStaCode=&goalCompId=&_=&startStaCode='+stationinfo['StationCode']+'&startCompId='+stationinfo['CompanyID'],data=json.dumps({'Content-Type': 'application/json'}).encode())
+    noribas=json.loads(urllib.request.urlopen(req).read().decode())
+    
+    return render_template('stop.html',kurukendata=kurukendata,noribas=json.dumps(noribas))
 
 if __name__ == '__main__':
   app.run()
--- api/trip.py
+++ api/trip.py
@@ -14,11 +14,12 @@ def stop():
         stationsid=request.args.get('stationsid')
     req=urllib.request.Request('https://kuruken.jp/Approach/BusLocationByCourseBusId?daiyaSid='+daiyasid,data=json.dumps({'Content-Type': 'application/json'}).encode())
     jsondata=urllib.request.urlopen(req).read().decode().replace("\\r\\n","\n").replace("\\u003c","<").replace("\\u003e",">").replace('\\"','"').replace("\\u0027","'").replace("<script","<!--<script").replace("</script>","</script>-->")[1:-1]
-    return render_template('trip.html',jsondata=jsondata if jsondata else "{}")
+    req=urllib.request.Request('https://kuruken.jp/Approach/ChangeNoriba',json.dumps({'selectLang':'ja','stationSid':stationsid,'goalStaCode':'','goalComId':'','listSortMode':'1'}).encode(),{'Content-Type': 'application/json'})
+    kurukendata=urllib.request.urlopen(req).read().decode().replace("\\r\\n","\n").replace("\\u003c","<").replace("\\u003e",">").replace('\\"','"').replace("\\u0027","'").replace("<script","<!--<script").replace("</script>","</script>-->")[1:-1]
+    return render_template('trip.html',kurukendata=kurukendata,jsondata=jsondata if jsondata else "{}")
     req=urllib.request.Request('https://kuruken.jp/ViewTimeTable/RouteDetail?selectLang=ja&ikisakiSEQ=0&selectedTime=0&parentCompanyCode=73000000&stationSid='+stationsid+'&DaiyaSid='+daiyasid,data=json.dumps({'Content-Type': 'application/json'}).encode())
     #return render_template('trip.html',jsondata=jsondata if jsondata else "{}",response=urllib.request.urlopen(req).read().decode().replace("\\r\\n","\n").replace("\\u003c","<").replace("\\u003e",">").replace('\\"','"').replace("\\u0027","'").replace("<script","<!--<script").replace("</script>","</script>-->"))
 
 
 if __name__ == '__main__':
   app.run()
-  
